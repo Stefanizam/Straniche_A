04ad81127a4bfdf53d8be329bc1fefffc156a235
Stefan <bozinovski.stefan@gmail.com>
2021-03-24 22:05:23 +0100

    24-03_22-05

--- main_script.js
+++ main_script.js
@@ -1,16 +1,25 @@
 let skillsContainer = document.querySelector("#vfxSkillsContainer");
 let skillsDiv = document.querySelectorAll('#vfxSkillsContainer>div');
+const skillsHeight = document.querySelector('#vfxSkillsContainer>div').style.height;
 
-for (let divche of skillsDiv) {
-    divche.addEventListener('mouseenter', () => {
-        divche.style.height = "200px";
-        divche.querySelector("p").style.display = "flex";
-    });
+let resetDivs = function () {  // For later use
+    for (let divche of skillsDiv) {
+        divche.style.height = skillsHeight;
+        divche.querySelector("p").style.display = "none";
+    }
 }
-
 for (let divche of skillsDiv) {
+    let clickSwitch = 0;
+    divche.addEventListener('mouseenter', () => {
+        divche.style.height = "240px";
+        divche.querySelector("p").style.display = "flex";
+        clickSwitch = 1;
+    });
+
     divche.addEventListener('mouseleave', () => {
-            divche.style.height = "80px";
-            divche.querySelector("p").style.display = "none";
-        });
+        divche.style.height = skillsHeight;
+        divche.querySelector("p").style.display = "none";
+        clickSwitch = 0;
+    });
+
 }
