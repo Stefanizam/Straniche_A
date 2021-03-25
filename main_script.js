2e2697b74c067dcf9f76e19fc64a8e9e5f0a60b1
Stefan <bozinovski.stefan@gmail.com>
2021-03-25 17:30:41 +0100

    25-03_17-30

--- main_script.js
+++ main_script.js
@@ -13,14 +13,38 @@ let resetDivs = function () {  // Ignore for now, for later use
 for (let divche of skillsDiv) {
     divche.addEventListener('mouseenter', () => {
         divche.style.height = "240px";
-        divche.querySelector("p").style.display = "inline";
+        divche.querySelector("p").style.display = "flex";
         divche.querySelector("p").style.animation = "0.5s linear 0s 1 opacityAppear";
-        divche.querySelector("p").style.backgroundImage = "../Images/pntImageB.jpg"
+        switch (divche.id) {
+            case "vfxPaint":
+                divche.querySelector(".wrapper").style.backgroundImage = "url(../Straniche_A/Images/pntGIF.gif)";
+                break;
+            case "vfxComp":
+                divche.querySelector(".wrapper").style.backgroundImage = "url(../Straniche_A/Images/compGIF.gif)";
+                break;
+            case "vfxMotionTrack":
+                divche.querySelector('video').style.height = "auto";
+                divche.querySelector('video').style.zIndex = "1";
+                divche.querySelector('video').play();
+                break;
+        }
     });
 
     divche.addEventListener('mouseleave', () => {
         divche.style.height = skillsHeight;
         divche.querySelector("p").style.display = "none";
+        switch (divche.id) {
+            case "vfxPaint":
+                divche.querySelector(".wrapper").style.backgroundImage = "url(../Straniche_A/Images/pntImage.jpg";
+                break;
+            case "vfxComp":
+                divche.querySelector(".wrapper").style.backgroundImage = "url(../Straniche_A/Images/compImage.jpg)";
+                break;
+            case "vfxMotionTrack":
+                divche.querySelector('video').style.height = "0px";
+                divche.querySelector('video').style.zIndex = "-1";
+                divche.querySelector('video').pause();
+                break;
+        }
     });
-
 }
