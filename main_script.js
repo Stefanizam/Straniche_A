c728817afa9b68e9c9306cf94ae6c5b5d07a6f98
Stefan <bozinovski.stefan@gmail.com>
2021-03-27 01:15:44 +0100

    Updated VFX Videos

--- main_script.js
+++ main_script.js
@@ -9,16 +9,10 @@ for (let divche of skillsDiv) {
         divche.querySelector("p").style.display = "flex";
         divche.querySelector("p").style.animation = "0.5s linear 0s 1 opacityAppear";
 
-        switch (divche.id) {
-            case "vfxPaint":
-                divche.querySelector(".wrapper").style.backgroundImage = "url(../Straniche_A/Images/pntGIF.gif)"
-                break;
+        divche.querySelector(".wrapper").style.backgroundImage = "unset";
+        divche.querySelector('video').style.height = "unset";
+        divche.querySelector('video').play();
 
-            default:
-                divche.querySelector(".wrapper").style.backgroundImage = "unset";
-                divche.querySelector('video').style.height = "unset";
-                divche.querySelector('video').play();
-        }
 
     });
 
@@ -35,19 +29,19 @@ for (let divche of skillsDiv) {
 
         switch (divche.id) {
             case "vfxPaint":
-                hideVideo("vfxPaintImage.jpg");
+                hideVideo("vfxPaint.jpg");
                 break;
             case "vfxComp":
-                hideVideo("compImage.jpg");
+                hideVideo("vfxComp.jpg");
                 break;
             case "vfxMotionTrack":
-                hideVideo("motionTrackImage.jpg");
+                hideVideo("vfxCMM.jpg");
                 break;
             case "vfxRoto":
                 hideVideo("vfxRoto.jpg");
                 break;
             case "vfxSimulation":
-                hideVideo("motionTrackImage.jpg");
+                hideVideo("vfxSim.jpg");
                 break;
             case "vfx3D":
                 hideVideo("vfxRoto.jpg");
