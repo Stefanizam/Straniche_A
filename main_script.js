e73459c3284f6ca22d38ed8029cabff193480404
Stefanizam <bozinovski.stefan@gmail.com>
2021-03-28 12:42:51 +0200

    Added vfx Icons

--- main_script.js
+++ main_script.js
@@ -1,3 +1,4 @@
+// vfx Skills Hover actions
 let skillsContainer = document.querySelector("#vfxSkillsContainer");
 let skillsDiv = document.querySelectorAll('#vfxSkillsContainer>div');
 const skillsHeight = document.querySelector('#vfxSkillsContainer>div').style.height;
@@ -7,6 +8,7 @@ for (let divche of skillsDiv) {
 
         divche.style.height = "240px";
         divche.querySelector("p").style.display = "flex";
+        divche.querySelector(".iconsContainer").style.display = "flex";        
         divche.querySelector("p").style.animation = "0.5s linear 0s 1 opacityAppear";
 
         divche.querySelector(".wrapper").style.backgroundImage = "unset";
@@ -18,6 +20,7 @@ for (let divche of skillsDiv) {
 
         divche.style.height = skillsHeight;
         divche.querySelector("p").style.display = "none";
+        divche.querySelector(".iconsContainer").style.display = "none";
 
         let hideVideo = function (image) {
             divche.querySelector(".wrapper").style.backgroundImage = `url(../Straniche_A/Images/${image})`;
@@ -47,3 +50,4 @@ for (let divche of skillsDiv) {
         }
     });
 }
+// vfx Skills Hover - End
