67cc94b2ca29d666c9eaa2bad7d72949c38b488a
Stefanizam <bozinovski.stefan@gmail.com>
2021-03-28 17:54:10 +0200

    fix videos

--- main_script.js
+++ main_script.js
@@ -13,7 +13,6 @@ for (let divche of skillsDiv) {
         divche.querySelector(".iconsContainer").style.display = "flex";        
         divche.querySelector("p").style.display = "flex";
         divche.querySelector("p").style.animation = "0.5s linear 0s 1 opacityAppear";
-        divche.querySelector('video').style.display = "flex"
         divche.querySelector('video').play();
     });
 
@@ -22,33 +21,7 @@ for (let divche of skillsDiv) {
         divche.style.height = skillsHeight;
         divche.querySelector("p").style.display = "none";
         divche.querySelector(".iconsContainer").style.display = "none";
-
-        let hideVideo = function (image) {
-            divche.querySelector(".wrapper").style.backgroundImage = `url(../Straniche_A/Images/${image})`;
-            divche.querySelector('video').pause();
-            divche.querySelector('video').style.display = "none";
-        }
-
-        switch (divche.id) {
-            case "vfxPaint":
-                hideVideo("vfxPaint.jpg");
-                break;
-            case "vfxComp":
-                hideVideo("vfxComp.jpg");
-                break;
-            case "vfxMotionTrack":
-                hideVideo("vfxCMM.jpg");
-                break;
-            case "vfxRoto":
-                hideVideo("vfxRoto.jpg");
-                break;
-            case "vfxSimulation":
-                hideVideo("vfxSim.jpg");
-                break;
-            case "vfx3D":
-                hideVideo("vfx3D.jpg");
-                break;
-        }
+        divche.querySelector('video').pause();
     });
 }
 // vfx Skills Hover - End
