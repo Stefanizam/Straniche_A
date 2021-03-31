fbefaacabef7a3a3e2204ee44b17311bab264348
Stefanizam <37158682+Stefanizam@users.noreply.github.com>
2021-04-01 00:56:54 +0200

    skills reworked

--- main_script.js
+++ main_script.js
@@ -1,23 +1,21 @@
 // vfx Skills Hover actions
 let skillsContainer = document.querySelector(".skillsContainer");
-let skillsDiv = document.querySelectorAll('.skillsContainer>div');
-const skillsHeight = document.querySelector('.skillsContainer>div').style.height;
+let skillsDiv = document.querySelectorAll('.wrapper');
+const skillsHeight = document.querySelector('.wrapper').style.height;
 
 for (let divche of skillsDiv) {
 
     divche.addEventListener('mouseenter', () => {
 
-        divche.style.height = "300px";
-        divche.querySelector(".wrapper").style.backgroundImage = "unset";
+        // divche.style.height = "300px";
         divche.querySelector(".iconsContainer").style.display = "flex";
         divche.querySelector("p").style.display = "flex";
-        divche.querySelector("p").style.animation = "0.5s linear 0s 1 opacityAppear";
         divche.querySelector('video').play();
     });
 
     divche.addEventListener('mouseleave', () => {
 
-        divche.style.height = skillsHeight;
+        // divche.style.height = skillsHeight;
         divche.querySelector("p").style.display = "none";
         divche.querySelector(".iconsContainer").style.display = "none";
         divche.querySelector('video').pause();
