df603b44044c427bd06649ee3b869f41d953e5fb
Stefanizam <bozinovski.stefan@gmail.com>
2021-04-10 01:01:01 +0200

    affinity section

--- main_script.js
+++ main_script.js
@@ -178,7 +178,7 @@ const navButton = document.querySelector("#skillsBtn");
 const subNavContent = document.querySelector("#subNavContent");
 const subNavElements = document.querySelectorAll('#subNavContent>a');
 
-navButton.addEventListener('mouseenter', () => {
+navButton.addEventListener('mousedown', () => {
     subNavContent.style.visibility = "visible";
     subNavContent.style.opacity = "1";
     subNavContent.style.transform = "translateY(0em)";
@@ -187,7 +187,6 @@ navButton.addEventListener('mouseenter', () => {
 subNavContent.addEventListener('mouseleave', () => {
     subNavContent.style.visibility = "hidden";
     subNavContent.style.opacity = "0";
-
     subNavContent.style.transform = "translateY(-12em)";
 })
 
@@ -204,10 +203,57 @@ buttonCV.addEventListener('mouseleave', () => {
 })
 
 
-// Background Parallax
+// Background Parallax  -- Problematic on different devices -- Needs improvement
 window.addEventListener('scroll', () => {
     let pageContainer = document.getElementById('pageContainer');
     let scrolled = window.pageYOffset / 2;
 
     pageContainer.style.backgroundPosition = `0px ${scrolled}px`;
 })
+
+
+// Software Affinity stuff
+
+// Icon Creator
+let softContainer = document.querySelectorAll('.softContainer');
+
+for (let container of softContainer) {
+
+    // Pull name and percentage from HTML dataset
+    let pullName = container.dataset.name;
+    let pullPercentage = container.dataset.percentage;
+
+    // Create Image Tag and Link it
+    let imgTag = document.createElement('img');
+    imgTag.src = `Images/Icons/ico_${pullName}.png`;
+    imgTag.alt = pullName;
+    container.appendChild(imgTag);
+
+    // Create and Append Software Name
+    let softInfoDiv = document.createElement('div');
+    softInfoDiv.classList.add('softInfo');
+    let nameP = document.createElement('p');
+    let softName = document.createTextNode(`${pullName}`);
+    nameP.appendChild(softName);
+    softInfoDiv.appendChild(nameP);
+    container.appendChild(softInfoDiv);
+
+    // Create and Append XP Bar
+    let xpBarBkg = document.createElement('div');
+    xpBarBkg.classList.add('xpBkg');
+    softInfoDiv.appendChild(xpBarBkg);
+    let xpBarFill = document.createElement('div');
+    xpBarFill.classList.add('xpFill', 'font-XS');
+    xpBarFill.style.width = `${pullPercentage}%`;
+    let percentageP = document.createElement('p');
+    let percentageNum = document.createTextNode(pullPercentage);
+    percentageP.appendChild(percentageNum);
+    xpBarFill.appendChild(percentageP);
+    xpBarBkg.appendChild(xpBarFill);
+}
+
+// for (let container of softContainer) {
+//     container.addEventListener('mouseenter', () => {
+//         container.querySelector('.xpFill').style.transform = 'scale(1,1)';
+//     })
+// }
