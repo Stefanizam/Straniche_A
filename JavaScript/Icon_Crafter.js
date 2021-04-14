8d8a96138980e187cdd43fb1f540c568d1c82bd2
Stefanizam <bozinovski.stefan@gmail.com>
2021-04-14 19:16:39 +0200

    JS organise

---
+++ JavaScript/Icon_Crafter.js
@@ -0,0 +1,41 @@
+// Icon Crafter
+
+function craftIcons() {
+
+    let softContainer = document.querySelectorAll('.softContainer');
+
+    for (let container of softContainer) {
+
+        // Pull name and percentage from HTML dataset
+        let pullName = container.dataset.name;
+        let pullPercentage = container.dataset.percentage;
+
+        // Create Image Tag and Link it
+        let imgTag = document.createElement('img');
+        imgTag.src = `Images/Icons/ico_${pullName}.svg`;
+        imgTag.alt = pullName;
+        container.appendChild(imgTag);
+
+        // Create and Append Software Name
+        let softInfoDiv = document.createElement('div');
+        softInfoDiv.classList.add('softInfo');
+        let nameP = document.createElement('p');
+        let softName = document.createTextNode(`${pullName}`);
+        nameP.appendChild(softName);
+        softInfoDiv.appendChild(nameP);
+        container.appendChild(softInfoDiv);
+
+        // Create and Append XP Bar
+        let xpBarBkg = document.createElement('div');
+        xpBarBkg.classList.add('xpBkg');
+        softInfoDiv.appendChild(xpBarBkg);
+        let xpBarFill = document.createElement('div');
+        xpBarFill.classList.add('xpFill', 'font-XS');
+        xpBarFill.style.width = `${pullPercentage}%`;
+        let percentageP = document.createElement('p');
+        let percentageNum = document.createTextNode(pullPercentage);
+        percentageP.appendChild(percentageNum);
+        xpBarFill.appendChild(percentageP);
+        xpBarBkg.appendChild(xpBarFill);
+    }
+}
--- JavaScript/main_script.js
+++ JavaScript/main_script.js
@@ -140,56 +140,20 @@ buttonCV.addEventListener('mouseleave', () => {
 })
 
 
-// Background Parallax  -- Problematic on different devices -- Needs improvement
-function bkgParallax() {
-    window.addEventListener('scroll', () => {
-        let scrolledBkg = window.pageYOffset;
+// Background Parallax  -- Problematic on different devices -- OMMITED
+// function bkgParallax() {
+//     window.addEventListener('scroll', () => {
+//         let scrolledBkg = window.pageYOffset;
 
-        let pageContainer = document.getElementById('pageContainer');
-        pageContainer.style.backgroundPosition = `0px ${scrolledBkg / 1.5}px`;
-    })
-}
-window.requestAnimationFrame(bkgParallax);
+//         let pageContainer = document.getElementById('pageContainer');
+//         pageContainer.style.backgroundPosition = `0px ${scrolledBkg / 1.5}px`;
+//     })
+// }
+// window.requestAnimationFrame(bkgParallax);
 
 
 // Software Affinity stuff
-// Icon Crafter
-let softContainer = document.querySelectorAll('.softContainer');
-
-for (let container of softContainer) {
-
-    // Pull name and percentage from HTML dataset
-    let pullName = container.dataset.name;
-    let pullPercentage = container.dataset.percentage;
-
-    // Create Image Tag and Link it
-    let imgTag = document.createElement('img');
-    imgTag.src = `Images/Icons/ico_${pullName}.svg`;
-    imgTag.alt = pullName;
-    container.appendChild(imgTag);
-
-    // Create and Append Software Name
-    let softInfoDiv = document.createElement('div');
-    softInfoDiv.classList.add('softInfo');
-    let nameP = document.createElement('p');
-    let softName = document.createTextNode(`${pullName}`);
-    nameP.appendChild(softName);
-    softInfoDiv.appendChild(nameP);
-    container.appendChild(softInfoDiv);
-
-    // Create and Append XP Bar
-    let xpBarBkg = document.createElement('div');
-    xpBarBkg.classList.add('xpBkg');
-    softInfoDiv.appendChild(xpBarBkg);
-    let xpBarFill = document.createElement('div');
-    xpBarFill.classList.add('xpFill', 'font-XS');
-    xpBarFill.style.width = `${pullPercentage}%`;
-    let percentageP = document.createElement('p');
-    let percentageNum = document.createTextNode(pullPercentage);
-    percentageP.appendChild(percentageNum);
-    xpBarFill.appendChild(percentageP);
-    xpBarBkg.appendChild(xpBarFill);
-}
+craftIcons();
 
 // xpBars Animation
 (function () {
